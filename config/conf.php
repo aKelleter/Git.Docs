fd2ce7d4c7c78806db1c564089a1a185cb4147c2
Alain Kelleter <alain.kelleter@kelleter.be>
2025-06-18 20:50:55 +0200

    Update conf.php

--- config/conf.php
+++ config/conf.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 const APP_NAME = "Git.Docs";
 const APP_VER = "1.9.0-18062025-1630";
-const BASE_URL = '/prj/Git.Docs';
+const BASE_URL = '/Git.Docs';
 const DEFAULT_MODULE = 'git';
 
 const APP_MULTI_LANG = true;
